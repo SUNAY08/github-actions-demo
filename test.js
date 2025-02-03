8df8d6e74872a1db4adb4fc28cd4ee79edaf2fcc
sunilrautelasunay <sunil.rautela@monotype.com>
2025-02-03 23:36:20 +0530

    fix: correct expected response text in test case

--- test.js
+++ test.js
@@ -3,6 +3,6 @@ const request = require('supertest');
 describe('GET /', () => {
   it('responds with "Hello CI/CD!"', async () => {
     const response = await request(app).get('/');
-    expect(response.text).toBe('Hello CI/CD!!');
+    expect(response.text).toBe('Hello CI/CD!');
   });
 });
